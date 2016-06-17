e44d41409f1d2398ee6c0e8d7ce6add13b1fd3d6
Kevin Vaysse <vayssek@gmail.com>
2016-06-17 14:15:47 +0200

    UPDATE StockService.java for methode deleteStock()
    return a Response

--- src/main/java/com/lunaret_seb/hb_lunaret_seb_zoo/stock/StockService.java
+++ src/main/java/com/lunaret_seb/hb_lunaret_seb_zoo/stock/StockService.java
@@ -3,10 +3,16 @@ package com.lunaret_seb.hb_lunaret_seb_zoo.stock;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.Response.Status;
 
 @Path("/")
 public class StockService {
@@ -35,4 +41,14 @@ public class StockService {
         
 		return selectedStock;
 	}
+	
+	@DELETE
+	@Path("/stocks/delete/")
+	@Consumes({ "application/json" })
+	public Response deleteStock(Entity E){
+		return Response.status(Status.ACCEPTED).build();
+	}
+	
+	
+	
 }
