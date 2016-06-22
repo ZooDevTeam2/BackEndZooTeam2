0a77579df1f5848c3397180fb493474f6692d31c
Fadul <fadul.fatouma@gmail.com>
2016-06-22 15:41:45 +0200

    CREATE a Service for Tickets

--- src/main/java/com/lunaret_seb/hb_lunaret_seb_zoo/ticket/TicketRest.java
+++ src/main/java/com/lunaret_seb/hb_lunaret_seb_zoo/ticket/TicketRest.java
@@ -1,68 +1,58 @@
 package com.lunaret_seb.hb_lunaret_seb_zoo.ticket;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.client.Entity;
 
 @Path("/")
 public class TicketRest {
 	
+	@Inject 
+	ITicket DB;
+	
 	@GET
 	@Path("/tickets")
-	@Produces({ "application/json"})
-	public List<Ticket> listTicket(){
-		List<Ticket>listTicket = new ArrayList<Ticket>();
-		listTicket.add(new Ticket("Tarif normal", 8, 1));
-		listTicket.add(new Ticket("Tarif réduit", 6, 2));
-		listTicket.add(new Ticket("Tarif enfant", 5, 3));
-		listTicket.add(new Ticket("Tarif carnet 10", 65, 4));
-		listTicket.add(new Ticket("Tarif groupe adultes", 55, 5));
-		listTicket.add(new Ticket("Tarif groupe scolaire", 80, 6));
-		listTicket.add(new Ticket("Tarif groupe étudiant", 85, 7));
-		return listTicket;
+	@Produces({ "application/json" })
+	public List<Ticket> getTicket() {
+		return DB.getAll();
 	}
 	
 	@GET
 	@Path("/tickets/{id}")
-	@Produces({ "application/json"})
-	public Ticket detailTicket(@PathParam("id") int id) {
-		List<Ticket>listTicket = new ArrayList<Ticket>();
-		listTicket.add(new Ticket("Tarif normal", 8, 1));
-		listTicket.add(new Ticket("Tarif réduit", 6, 2));
-		listTicket.add(new Ticket("Tarif enfant", 5, 3));
-		listTicket.add(new Ticket("Tarif carnet 10", 65, 4));
-		listTicket.add(new Ticket("Tarif groupe adultes", 55, 5));
-		listTicket.add(new Ticket("Tarif groupe scolaire", 80, 6));
-		listTicket.add(new Ticket("Tarif groupe étudiant", 85, 7));
-
-		
-		Ticket selectedTicket = listTicket.get(id-1);
-		return selectedTicket;
+	@Produces({ "application/json" })
+	public Ticket getTicketByIndex(@PathParam("id") int id) {
+		return DB.getByIndex(id);
 	}
 	
 	@DELETE
-	@Path("/tickets/delete/")
-	@Consumes({ "application/json" })
-	public Response deleteTicket(Entity E) {
-		return Response.status(Status.ACCEPTED).build();
+	@Path("/tickets/delete/{id}")
+	@Produces("application/json")
+	public Response deleteTicket(@PathParam("id") int id) {
+		DB.remove(id);
+		List<Ticket> wrapThis = DB.getAll();
+		return Response.status(200).entity(wrapThis).build();
 	}
 	
 	@PUT
 	@Path("/tickets/update/")
-	@Consumes({ "application/json"})
-	public Response updateTicket(Entity E){
-		return Response.status(Status.ACCEPTED).build();
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response updateTicket(Ticket ticket, @PathParam("id") int id){
+		DB.update(ticket, id);
+		List<Ticket> wrapThis = DB.getAll();
+		return Response.status(200).entity(wrapThis).build();
 	}
 	
 	@POST
 	@Path("/tickets/add/")
-	@Consumes({ "application/json"})
-	public Response addTicket(Entity E){
-		return Response.status(Status.ACCEPTED).build();
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response addTicket(Ticket ticket){
+		DB.add(ticket);;
+		List<Ticket> wrapThis = DB.getAll();
+		return Response.status(200).entity(wrapThis).build();
 	}
 
 }
